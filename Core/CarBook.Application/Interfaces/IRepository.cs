d2e181acdeff44f4e2b14c66a386d41996511ec2
talhaondrr <talhanuri@icloud.com>
2024-09-13 10:12:17 +0300

    first

---
+++ Core/CarBook.Application/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Interfaces
+{
+    internal interface IRepository <T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync();
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsyncs(T entity);
+    }
+}
